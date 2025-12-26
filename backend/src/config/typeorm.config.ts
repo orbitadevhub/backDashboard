0e8d527eaf24467ce2d1a8e7c0e1cf55a00e3961
Martín Valdés <valdesmartin@gmail.com>
2025-12-26 14:54:23 -0300

    fix: use npx for nest build

--- backend/src/config/typeorm.config.ts
+++ backend/src/config/typeorm.config.ts
@@ -4,22 +4,31 @@ import { join } from 'path';
 
 export const typeOrmConfig = (
   configService: ConfigService,
-): TypeOrmModuleOptions => ({
-  type: 'postgres',
-  host: configService.get('DATABASE_HOST'),
-  port: configService.get('DATABASE_PORT'),
-  username: configService.get('DATABASE_USER'),
-  password: configService.get('DATABASE_PASS'),
-  database: configService.get('DATABASE_NAME'),
-  //   ssl: true,
-  entities: [join(__dirname + '../../**/*.entity.{js,ts}')],
-  synchronize: true,
-  ssl: false,
-  extra: {
-    ssl:false
-    //  {
-    //   rejectUnauthorized: false,
-    // },
-  },
-  
-});
+): TypeOrmModuleOptions => {
+  const isProduction = configService.get('NODE_ENV') === 'production';
+
+  return {
+    type: 'postgres',
+
+    // ✅ PRODUCCIÓN (Render + Neon)
+    ...(isProduction
+      ? {
+          url: configService.get<string>('DATABASE_URL'),
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        }
+      : {
+          // ✅ LOCAL
+          host: configService.get<string>('DATABASE_HOST'),
+          port: Number(configService.get('DATABASE_PORT')),
+          username: configService.get<string>('DATABASE_USER'),
+          password: configService.get<string>('DATABASE_PASS'),
+          database: configService.get<string>('DATABASE_NAME'),
+          ssl: false,
+        }),
+
+    entities: [join(__dirname, '../../**/*.entity.{js,ts}')],
+    synchronize: true, // ⚠️ solo dev; en prod ideal false
+  };
+};
