f0d1acbc8cbc895b6f8f28c57ea8efb1f1ad9103
Martín Valdés <valdesmartin@gmail.com>
2025-12-26 23:23:19 -0300

    neon db ok

--- backend/src/qremail/qremail.service.ts
+++ backend/src/qremail/qremail.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import * as nodemailer from 'nodemailer';
 
-
 @Injectable()
 export class QremailService {
   private transporter: nodemailer.Transporter;
@@ -15,31 +14,33 @@ export class QremailService {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
       },
+      tls: {
+        rejectUnauthorized: false,
+      },
     });
   }
 
   async send2FAQRCode(email: string, qrBase64: string) {
-  const base64Data = qrBase64.replace(/^data:image\/png;base64,/, '');
+    const base64Data = qrBase64.replace(/^data:image\/png;base64,/, '');
 
-  await this.transporter.sendMail({
-    from: `"MiApp Seguridad" <${process.env.MAIL_USER}>`,
-    to: email,
-    subject: 'Activación de doble factor (2FA)',
-    html: `
+    await this.transporter.sendMail({
+      from: `"MiApp Seguridad" <${process.env.MAIL_USER}>`,
+      to: email,
+      subject: 'Activación de doble factor (2FA)',
+      html: `
       <h3>Activación de doble factor</h3>
       <p>Escaneá este código con Google Authenticator o Authy:</p>
       <img src="cid:qrcode" />
       <p><strong>No compartas este código.</strong></p>
     `,
-    attachments: [
-      {
-        filename: 'qrcode.png',
-        content: base64Data,
-        encoding: 'base64',
-        cid: 'qrcode',
-      },
-    ],
-  });
-}
-
+      attachments: [
+        {
+          filename: 'qrcode.png',
+          content: base64Data,
+          encoding: 'base64',
+          cid: 'qrcode',
+        },
+      ],
+    });
+  }
 }
