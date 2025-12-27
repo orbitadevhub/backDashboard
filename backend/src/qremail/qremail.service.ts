ad280d0a5f6c419aee8c8aca254eda2e4d9ba670
Martín Valdés <valdesmartin@gmail.com>
2025-12-27 12:35:22 -0300

    neon db ok

--- backend/src/qremail/qremail.service.ts
+++ backend/src/qremail/qremail.service.ts
@@ -1,25 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { Resend } from 'resend';
+import { CloudinaryService } from 'src/config/cloudinary.service';
 
 @Injectable()
 export class QremailService {
+  constructor(private readonly cloudinaryService: CloudinaryService) {}
+
   private resend = new Resend(process.env.RESEND_API_KEY);
 
   async send2FAQRCode(email: string, qrBase64: string) {
+    const upload = await this.cloudinaryService.uploadBase64Image(
+      qrBase64,
+      '2fa'
+    );
+
     await this.resend.emails.send({
       from: 'onboarding@resend.dev',
       to: email,
       subject: 'Activación de doble factor (2FA)',
       html: `
-      <h3>Activación de doble factor</h3>
-      <p>Escaneá este código con Google Authenticator o Authy:</p>
-      <img 
-        src="${qrBase64}" 
-        alt="QR 2FA"
-        style="width:200px;height:200px"
-      />
-      <p><strong>No compartas este código.</strong></p>
-    `,
+        <h3>Activación de doble factor</h3>
+        <p>Escaneá este código:</p>
+        <img src="${upload.secure_url}" width="250" />
+        <p><strong>No compartas este código.</strong></p>
+      `,
     });
   }
 }
--- backend/src/twofa/twofactor.module.ts
+++ backend/src/twofa/twofactor.module.ts
@@ -5,12 +5,13 @@ import { UsersModule } from 'src/users/users.module';
 import { TwoFactorController } from './twofactor.controller';
 import { TwoFAGuard } from './twofactor.guard';
 import { QremailModule } from 'src/qremail/qremail.module';
+import { CloudinaryService } from 'src/config/cloudinary.service';
 
 
 @Module({
   imports: [UsersModule, JwtModule.register({}), QremailModule],
   controllers: [TwoFactorController],
-  providers: [TwoFactorAuthService, TwoFAGuard],
+  providers: [TwoFactorAuthService, TwoFAGuard, CloudinaryService],
   exports: [TwoFactorAuthService],
 })
 export class TwoFAModule {}
