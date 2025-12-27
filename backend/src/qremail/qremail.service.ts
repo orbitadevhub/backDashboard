d13d2a9fce0b0a1a3de740659e452d2188b6f1bb
Martín Valdés <valdesmartin@gmail.com>
2025-12-27 01:01:36 -0300

    neon db ok

--- backend/src/qremail/qremail.service.ts
+++ backend/src/qremail/qremail.service.ts
@@ -6,24 +6,16 @@ export class QremailService {
   private resend = new Resend(process.env.RESEND_API_KEY);
 
   async send2FAQRCode(email: string, qrBase64: string) {
-    const base64Data = qrBase64.replace(/^data:image\/png;base64,/, '');
-
     await this.resend.emails.send({
-      from: `"MiApp Seguridad" <${process.env.MAIL_USER}>`,
+      from: process.env.MAIL_FROM!, 
       to: email,
       subject: 'Activación de doble factor (2FA)',
       html: `
-      <h3>Activación de doble factor</h3>
-      <p>Escaneá este código con Google Authenticator o Authy:</p>
-      <img src="cid:qrcode" />
-      <p><strong>No compartas este código.</strong></p>
-    `,
-      attachments: [
-        {
-          filename: 'qrcode.png',
-          content: base64Data,
-        },
-      ],
+        <h3>Activación de doble factor</h3>
+        <p>Escaneá este código con Google Authenticator o Authy:</p>
+        <img src="${qrBase64}" alt="QR 2FA" />
+        <p><strong>No compartas este código.</strong></p>
+      `,
     });
   }
 }
