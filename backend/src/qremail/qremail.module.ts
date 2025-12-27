940797f50d0033289dce1c81d439e3393c6e2a18
Martín Valdés <valdesmartin@gmail.com>
2025-12-27 12:44:34 -0300

    neon db ok

--- backend/src/qremail/qremail.module.ts
+++ backend/src/qremail/qremail.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { QremailService } from './qremail.service';
+import { CloudinaryService } from 'src/config/cloudinary.service';
 
 @Module({
+  imports: [CloudinaryService],
   providers: [QremailService],
   exports: [QremailService],
 })
